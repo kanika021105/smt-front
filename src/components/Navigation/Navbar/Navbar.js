2e515e2165a16e347ceba1ab201f95be017f4c09
Tabish Alam <tabishalam.shaikh@gmail.com>
2021-10-09 22:09:07 +0530

    v1.5.1 Improved Navigation Style added responsive style with some animation and fixed some known UI/UX issue

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -1,39 +1,57 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import classes from './Navbar.module.scss';
+import AuthContext from '../../../store/AuthContext';
 
-const NavbarComponent = () => (
-    <div className={classes.navbar}>
-        <div className={classes.logo}>
-            SMT Panel
-        </div>
+const NavbarComponent = () => {
+    const { websiteName } = useContext(AuthContext);
+    const [showHamburger, setShowHamburger] = useState(false);
 
-        <div className={classes.nav_link}>
-            <ul>
-                <li>
-                    <NavLink to="/">
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/services">
-                        Services
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login">
-                        Login
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/signup">
-                        SIGN-UP
-                    </NavLink>
-                </li>
-            </ul>
+    return (
+        <div className={classes.navbar}>
+            <div className={classes.logo}>
+                {websiteName || 'SMT Panel'}
+            </div>
+
+            <div className={classes.nav_link}>
+                <div
+                    className={classes.hamburger}
+                    role="none"
+                    onClick={() => { setShowHamburger(!showHamburger); }}
+                >
+                    <div className={classes.hamburger_line} />
+                    <div className={classes.hamburger_line} />
+                    <div className={classes.hamburger_line} />
+                </div>
+
+                <div className={showHamburger ? classes.navlink__holder : classes.hide_links}>
+                    <ul>
+                        <li>
+                            <NavLink to="/">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/services">
+                                Services
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/login">
+                                Login
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/signup">
+                                SIGN-UP
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default React.memo(NavbarComponent);
